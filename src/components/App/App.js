09337cde6a51e9b73a0838f6cf50c9e870f67ee0
Pavel Tsveroshvili <mahanpt13@gmail.com>
2022-09-14 15:01:07 +0300

    роуты

---
+++ src/components/App/App.js
@@ -0,0 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
+
+function App() {
+  return (
+    <CurrentUserContext.Provider>
+      <div className="page">
+        <Routes>
+          <Route exact path="/">
+            <Main />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+        </Routes>
+      </div>
+    </CurrentUserContext.Provider>
+  );
+}
+
+export default App;
